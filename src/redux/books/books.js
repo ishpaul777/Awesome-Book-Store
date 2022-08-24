b268913454cc0310bc8750eb069e2d899cae77ca
Ishpaul Singh <ishpaul207@gmail.com>
2022-08-24 16:57:34 +0530

    Refactor with createAsycThunk

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import baseUrl from './api';
 
 const SET_BOOKS = 'BOOK_STORE/BOOKS/SET_BOOK';
@@ -7,14 +8,14 @@ const REMOVE_BOOK = 'BOOK_STORE/BOOKS/REMOVE_BOOK';
 const books = [];
 export default function bookReducer(state = books, action) {
   switch (action.type) {
-    case SET_BOOKS: {
-      return action.books;
+    case `${SET_BOOKS}/fulfilled`: {
+      return action.payload;
     }
-    case ADD_BOOK: {
-      return [...state, action.book];
+    case `${ADD_BOOK}/fulfilled`: {
+      return [...state, action.payload];
     }
-    case REMOVE_BOOK: {
-      const newState = state.filter((book) => book.id !== action.id);
+    case `${REMOVE_BOOK}/fulfilled`: {
+      const newState = state.filter((book) => book.id !== action.payload);
       return newState;
     }
     default: {
@@ -23,62 +24,57 @@ export default function bookReducer(state = books, action) {
   }
 }
 
-export const SetBooks = (books) => ({
-  type: SET_BOOKS,
-  books,
-});
+export const addBookToServer = createAsyncThunk(
+  ADD_BOOK,
+  async (book) => {
+    const param = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        item_id: book.id,
+        title: book.title,
+        author: book.author,
+        category: book.category,
+      }),
 
-export const AddBook = (book) => ({
-  type: ADD_BOOK,
-  book,
-});
+    };
+    await fetch(`${baseUrl}books`, param);
+    return book;
+  },
+);
 
-export const RemoveBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
-});
+export const getBooksFromServer = createAsyncThunk(
+  SET_BOOKS,
+  async () => {
+    const response = await fetch(`${baseUrl}books`).then((response) => response.json());
+    const books = [];
+    const booksIds = Object.keys(response);
+    booksIds.map((key) => books.push({
+      id: key,
+      title: response[key][0].title,
+      author: response[key][0].author,
+      category: response[key][0].category,
+    }));
+    const sortedBooks = books.sort((a, b) => a.title.localeCompare(b.title));
+    return sortedBooks;
+  },
+);
 
-export const addBookToServer = (book) => async (dispatch) => {
-  dispatch(AddBook(book));
-  const param = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json; charset=UTF-8',
-    },
-    body: JSON.stringify({
-      item_id: book.id,
-      title: book.title,
-      author: book.author,
-      category: book.category,
-    }),
-
-  };
-  await fetch(`${baseUrl}books`, param);
-};
-
-export const getBooksFromServer = () => async (dispatch) => {
-  const response = await fetch(`${baseUrl}books`).then((response) => response.json());
-  const books = [];
-  const booksIds = Object.keys(response);
-  booksIds.map((key) => books.push({
-    id: key,
-    title: response[key][0].title,
-    author: response[key][0].author,
-    category: response[key][0].category,
-  }));
-  dispatch(SetBooks(books));
-};
-
-export const removeBookfromServer = (bookId) => async (dispatch) => {
-  dispatch(RemoveBook(bookId));
-  const param = {
-    method: 'DELETE',
-    body: JSON.stringify({
-      item_id: bookId,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  };
-  await fetch(`${baseUrl}books/${bookId}`, param);
-};
+export const removeBookfromServer = createAsyncThunk(
+  REMOVE_BOOK,
+  async (bookId) => {
+    const param = {
+      method: 'DELETE',
+      body: JSON.stringify({
+        item_id: bookId,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    };
+    await fetch(`${baseUrl}books/${bookId}`, param);
+    return bookId;
+  },
+);
